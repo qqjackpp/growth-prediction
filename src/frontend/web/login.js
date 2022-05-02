3f91a9c3dbe212127ca99322ba813745b952009d
YoonSeok <qqjackpp@ajou.ac.kr>
2022-05-02 19:10:47 +0900

    로그인 및 회원가입 1차 구현

--- src/frontend/web/login.js
+++ src/frontend/web/login.js
@@ -10,24 +10,24 @@
 	var pw = (String)(formData.get("pw"));
 	
 	if ((id.length<10)||(id.length>20)){
-		alert("잘못된 아이디입니다.")
+		alert("잘못된 아이디입니다.");
 		return ;
 	}
 	if ((pw.length<10)||(pw.length>20)){
-		alert("잘못된 비밀번호입니다.")
+		alert("잘못된 비밀번호입니다.");
 		return ;
 	}
 	for (i = 0; i < id.length ; i++){
 		var ch = id[i];
 		if ((ch < "a" || ch > "z") && (ch < "A" || ch > "Z") && (ch < "0" || ch > "9")){
-			alert("잘못돤 형식의 아이디입니다.")
+			alert("잘못돤 형식의 아이디입니다.");
 			return ;
 		}
 	}
 	for (j = 0; j < pw.length ; j++){
 		var ch = pw[j];
 		if ((ch < "a" || ch > "z") && (ch < "A" || ch > "Z") && (ch < "0" || ch > "9")){
-			alert("잘못돤 형식의 비밀번호입니다.")
+			alert("잘못돤 형식의 비밀번호입니다.");
 			return ;
 		}
 	}
